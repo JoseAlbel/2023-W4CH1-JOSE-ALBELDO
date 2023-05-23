895a380c85e4e29a24e23580dce13ecb9624f3ad
josealbeldo <rayracing@gmail.com>
2023-05-23 11:45:46 +0200

    Changes in files

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./Button";
+import { Gentleman } from "./Gentleman";
+import { Header } from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Button></Button>
+      <Gentleman></Gentleman>
+    </>
+  );
+}
+
+export default App;
